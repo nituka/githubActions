cbc6d71723e47b25ca0c557b21a052b4881afe43
nitu <nitukapo@cisco.com>
2025-10-22 14:06:23 +0530

    new tc

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def add(a, b):
     return a + b
 
+
 if __name__ == "__main__":
     print("Hello, GitHub Actions!")
     print("2 + 3 =", add(2, 3))
+    print("Testing CI/CD pipeline...")
--- test_main.py
+++ test_main.py
@@ -7,7 +7,9 @@ class TestMain(unittest.TestCase):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
-
+    def test_sub(self):
+        self.assertEqual(sub(3,3), 0)
+        self.assertEqual(sub(3,2), 2)
 
 if __name__ == "__main__":
     unittest.main()
